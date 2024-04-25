80c5bebe8bda81e21cb60949c30822276353666c
justbelieveinmyself <122427316+justbelieveinmyself@users.noreply.github.com>
2024-04-25 23:57:29 +0300

    Simple gui (not worked)

--- src/main/java/com/justbelieveinmyself/portscanner/Main.java
+++ src/main/java/com/justbelieveinmyself/portscanner/Main.java
@@ -6,9 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
         GUI gui = null;
-        try {
             gui = new GUI();
-        } catch (Exception e) {}
+            gui.setVisible(true);
     }
 
 }
